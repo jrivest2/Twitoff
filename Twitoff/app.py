356ca50bdf98b00ed7808c914ae5ce70e79f4680
jrivest2 <justinrivest@gmail.com>
2020-11-10 14:47:48 -0700

    Assignment 2

--- Twitoff/app.py
+++ Twitoff/app.py
@@ -1,24 +1,45 @@
 """Main app/routing file for Twitoff"""
-
-from flask import Flask, render_template
+from os import getenv
+from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
+# h
 
 
 def create_app():
     """Creating and configuring an instance of the Flask"""
     app = Flask(__name__)
     
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        # we must create the database
-        # DB.drop_all()
-        DB.create_all()
         # renders base.html template and passes down title and users
-        return render_template('base.html',title="home", users=User.query.all())
+        return render_template('base.html',title="Home", users=User.query.all())
+    @app.route('/update')  # http://127.0.0.1:5000/update
+    def update():
+        reset()
+        insert_example_users()
+        return render_template('base.html', title="Home", users=User.query.all())
+
+    @app.route('/reset')  # http://127.0.0.1:5000/reset
+    def reset():
+        # we must create the database
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title="Home")
+    
+    @app.route('/add',methods=["POST"])
+    def add_user():
+        if request.method == "POST":
+            username = request.form['text']
+            add_or_update_user(username)
+        return render_template('base.html',title="Home")
     return app
 
+def insert_example_users():
+    add_or_update_user('elonmusk')
+    add_or_update_user('nasa')
--- Twitoff/models.py
+++ Twitoff/models.py
@@ -9,13 +9,14 @@ class User(DB.Model):
     """Twitter User Table that will correspond to tweets = SQLAlchemy syntax"""
     id = DB.Column(DB.BigInteger, primary_key=True) # id column (primary key)
     name = DB.Column(DB.String, nullable=False) # name column
-    num_tweets = DB.Column(DB.BigInteger, nullable=True)
-    total_likes = DB.Column(DB.BigInteger, nullable=True)
+    newest_tweet_id = DB.Column(DB.BigInteger) # Keeps track of recent tweet
+    # num_tweets = DB.Column(DB.BigInteger, nullable=True)
+    # total_likes = DB.Column(DB.BigInteger, nullable=True)
     def __init__(self,id,name):
         self.id = id
         self.name = name
-        self.total_likes = self.count_likes()
-        self.num_tweets = len(self.tweets)
+        # self.total_likes = self.count_likes()
+        # self.num_tweets = len(self.tweets)
 
     def count_likes(self):
         count = 0
@@ -30,18 +31,19 @@ class Tweet(DB.Model):
     """Tweet text data - associated with Users Table"""
     id = DB.Column(DB.BigInteger, primary_key=True) # id column (primary key)
     text = DB.Column(DB.Unicode(300)) # Tweet text column
+    vect = DB.Column(DB.PickleType, nullable=False) # 
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey("user.id"), nullable=True)
     user= DB.relationship('User', backref=DB.backref('tweets', lazy=True))
-    likes = DB.Column(DB.BigInteger, nullable=True)
+    # likes = DB.Column(DB.BigInteger, nullable=True)
     
-    def __init__(self,id,text,user,likes):
-        # User.id.total_likes += likes
-        self.id = id
-        self.text = text
-        self.user = user
-        self.likes = likes
-        self.user.total_likes += self.likes
-        self.user.num_tweets += 1
+    # def __init__(self,id,text,user,likes=0):
+    #     # User.id.total_likes += likes
+    #     self.id = id
+    #     self.text = text
+    #     self.user = user
+    #     self.likes = likes
+    #     self.user.total_likes += self.likes
+    #     self.user.num_tweets += 1
 
     def __repr__(self):
         return "<Tweet: {}>".format(self.text)
